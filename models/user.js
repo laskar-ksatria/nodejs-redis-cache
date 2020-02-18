482f711e045e61948a49f220137b30ac9f851c4f
laskar-ksatria <dock_fest@yahoo.co.id>
2020-02-18 19:37:49 +0700

    adding-file2

--- models/user.js
+++ models/user.js
@@ -1,65 +1,56 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
 const { hashPass } = require('../helpers/hashPassword');
 
-const Schema = mongoose.Schema;
-
-const UserSchema = new Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Name is required'],
-    min: [true, 'Name must be between 2 to 30 chars']
+    required: [true, 'Name cannot be empty']
   },
   email: {
-    type: String,
-    validate: [{
-        validator: function (email) {
-            return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(email)
+        type: String,
+        validate: [{
+            validator: function (email) {
+                return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(email)
+            },
+            message: props => `${props.value} is not valid email format`
         },
-        message: props => `${props.value} is not valid email format`
+        {
+            validator: function (value) {
+                return this.model('User').findOne({email: value})
+                .then(function (email) {
+                    if (email) {
+                        return false
+                    }else {
+                        return true
+                    }
+                })
+            },
+            message: props => `${props.value} already taken, please take another one`
+        }
+    ],
+        required: [true, 'Email is required']
     },
-    {
-        validator: function (value) {
-            return this.model('User').findOne({email: value})
-            .then(function (email) {
-                if (email) {
-                    return false
-                }else {
-                    return true
-                }
-            })
-        },
-        message: props => `${props.value} already taken, please take another one`
-    }
-],
-    required: [true, 'Email is required']
-},
   password: {
     type: String,
-    required: [true, 'Password cannot be empty'],
-  },
-  avatar: {
-    type: String
-  },
-  account: { 
-    type: Schema.Types.ObjectId, ref: "Account" 
-  },
-  id_country: {
-    type: String,
-    default: ''
-  },
-  date: {
-    type: Date,
-    default: Date.now
+    required:[true, 'Password cannot be empty']
   }
-});
+})
 
-UserSchema.pre('save', function (next) {
+
+UserSchema.pre('save', function(next) {
   let pass = hashPass(this.password);
   this.password = pass;
   next();
 })
 
+const user = mongoose.model('User', UserSchema);
 
-const User = mongoose.model("User", UserSchema);
+module.exports = user;
 
-module.exports = User;
+
+
+
+
+
+
